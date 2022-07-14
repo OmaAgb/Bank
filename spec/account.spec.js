fc44a7f8b21a0129bfa7d28b7f147d69c3d1ae39
Oma Agbirigba <omaagb@gmail.com>
2022-07-14 21:08:42 +0100

    account test 2 pass

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -3,13 +3,30 @@ const Account = require('../src/account');
 
 describe(`Account Tests`, () => {
     //     // Test code for the wallet class goes here
-    it(`Test 1- default Account value is 0`, () => {
+    it(`Test 1 - default Account value is 0`, () => {
         // Arrange
         const account = new Account();
         const expected = 0;
         // Act
         const actual = account.getBalance();
-        //Assert
+        // Assert
         expect(actual).toBe(expected);
     });
+
+
+
+
+    it(`Test 2 - listOfTransactions array length is 0`, () => {
+        // Arrange
+        const account = new Account();
+        const expected = 0;
+        // Act
+        const actual = account.listOfTransactions.length
+        // Assert 
+        expect(actual).toBe(expected);
+    })
 });
+
+
+
+
